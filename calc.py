17a9e0adfaae6f8ccc98e762fd227cb45905db0a
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-08 13:50:06 +0100

    Add addition implementation

--- calc.py
+++ calc.py
@@ -2,5 +2,8 @@ from arithmetics import Arithmetics
 
 
 class Calc(Arithmetics):
+    def addition(self, a: float, b: float) -> float:
+        return a + b
+
     def difference(self, a: float, b: float) -> float:
         return a - b
