7c8cf0253aa15244c760347757600b7349a549de
Stanislaw Horna <stanislawhorna@outlook.com>
2024-12-08 13:41:18 +0100

    Feature lab1 zad3 (#15)
    Co-authored-by: rukaSUKII <sobonukasz@gmail.com>
    Co-authored-by: Grzegorz Kubicki <grzechu.kubicki@o2.pl>
    Co-authored-by: Grzegorz Kubicki <86533890+siemaEko@users.noreply.github.com>
    Co-authored-by: ŁukaszSob <95650572+rukaSUKII@users.noreply.github.com>
    Co-authored-by: abienias97 <96074651+abienias97@users.noreply.github.com>
    Co-authored-by: abienias97 <abienias97@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def print_id(github_id: str, role: str = ""):
     print(f'ZPI2024_zaoczni_NBP_Crasher {github_id} {role}')
 
+
 if __name__ == "__main__":
     print_id(github_id="GringoXY(Smithski)", role = "scrum master/developer")
+    print_id(github_id="siemaEko(Grzegorz Kubicki)")
+    print_id(github_id="StanislawHornaGitHub(Stanisław Horna)")
+    print_id(github_id="rukaSUKII(ŁukaszSob)")
+    print_id(github_id="abienias97")
