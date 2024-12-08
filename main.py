2164e68f22fa49cbb45e58fe9811dfa8e74e8401
rukaSUKII <sobonukasz@gmail.com>
2024-12-08 13:05:39 +0100

    Add displaying func. Display scrum master details

---
+++ main.py
@@ -0,0 +1,4 @@
+def print_id(github_id: str, role: str = ""):
+    print(f'ZPI2024_zaoczni_NBP_Crasher {github_id} {role}')
+
+print_id(github_id="GringoXY(Smithski)", role = "scrum master/developer")
