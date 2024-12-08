5d32c9bbe8ccee8b52b3080fca4a993866d0ebf4
Stanisław Horna <stanislawhorna@outlook.com>
2024-12-08 14:20:46 +0100

    Revert "add comment"
    This reverts commit 1d3c5c6ab719d518660494b24856e57e5dabb9ed.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def print_id(github_id: str, role: str = ""):
 if __name__ == "__main__":
     print_id(github_id="GringoXY(Smithski)", role = "scrum master/developer")
     print_id(github_id="siemaEko(Grzegorz Kubicki)")
-    # me
     print_id(github_id="StanislawHornaGitHub(Stanisław Horna)")
     print_id(github_id="rukaSUKII(ŁukaszSob)")
     print_id(github_id="abienias97")
