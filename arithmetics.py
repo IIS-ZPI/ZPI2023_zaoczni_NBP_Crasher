8b2533c41c4c64da0bf99786d6650da8cc41e0ba
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-08 13:49:52 +0100

    Add addition for interface

--- arithmetics.py
+++ arithmetics.py
@@ -3,5 +3,9 @@ from abc import ABC, abstractmethod
 
 class Arithmetics(ABC):
     @abstractmethod
+    def addition(self, a: float, b: float) -> float:
+        pass
+
+    @abstractmethod
     def difference(self, a: float, b: float) -> float:
         pass
