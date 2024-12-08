55f1e4b96c457652ff465460490f60a3f70b6c07
Stanisław Horna <stanislawhorna@outlook.com>
2024-12-08 13:48:29 +0100

    add Multiplication

--- arithmetics.py
+++ arithmetics.py
@@ -5,3 +5,7 @@ class Arithmetics(ABC):
     @abstractmethod
     def difference(self, a: float, b: float) -> float:
         pass
+    
+    @abstractmethod
+    def multiplication(self, a: float, b: float) -> float:
+        pass
