b900495b71b08b268c6908c898ab0d5656fd2850
ŁukaszSob <95650572+rukaSUKII@users.noreply.github.com>
2024-12-08 13:20:53 +0100

    feature lab1 zad3 241236 (#8)

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def print_id(github_id: str, role: str = ""):
     print(f'ZPI2024_zaoczni_NBP_Crasher {github_id} {role}')
 
+
 if __name__ == "__main__":
     print_id(github_id="GringoXY(Smithski)", role = "scrum master/developer")
     print_id(github_id="siemaEko(Grzegorz Kubicki)")
     print_id(github_id="StanislawHornaGitHub(Stanisław Horna)")
+    print_id(github_id="rukaSUKII(ŁukaszSob)")
