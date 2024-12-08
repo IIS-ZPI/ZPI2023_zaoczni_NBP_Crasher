2b19c3752b5fa0ce07e59dac3e50fdb5f28dae0b
rukaSUKII <sobonukasz@gmail.com>
2024-12-08 13:11:57 +0100

    Add my github_id

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def print_id(github_id: str, role: str = ""):
     print(f'ZPI2024_zaoczni_NBP_Crasher {github_id} {role}')
 
+
 if __name__ == "__main__":
-    print_id(github_id="GringoXY(Smithski)", role = "scrum master/developer")
+    print_id(github_id="GringoXY(Smithski)", role="scrum master/developer")
+    print_id(github_id="rukaSUKII(ŁukaszSob)")
