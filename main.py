5589052cdf9450f9b9f5da3357297a7e800777d1
Stanislaw Horna <stanislawhorna@outlook.com>
2024-12-08 13:35:15 +0100

    Feature lab1 zad2 (#14)
    Co-authored-by: rukaSUKII <sobonukasz@gmail.com>
    Co-authored-by: Grzegorz Kubicki <grzechu.kubicki@o2.pl>

---
+++ main.py
@@ -0,0 +1,5 @@
+def print_id(github_id: str, role: str = ""):
+    print(f'ZPI2024_zaoczni_NBP_Crasher {github_id} {role}')
+
+if __name__ == "__main__":
+    print_id(github_id="GringoXY(Smithski)", role = "scrum master/developer")
