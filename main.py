a7560a575d2f2470e73de92817ff502bf034d01f
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-08 13:09:24 +0100

    Add main guard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def print_id(github_id: str, role: str = ""):
     print(f'ZPI2024_zaoczni_NBP_Crasher {github_id} {role}')
 
-print_id(github_id="GringoXY(Smithski)", role = "scrum master/developer")
+if __name__ == "__main__":
+    print_id(github_id="GringoXY(Smithski)", role = "scrum master/developer")
