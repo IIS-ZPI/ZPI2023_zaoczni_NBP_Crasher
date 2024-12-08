68a0d609e91f074b47804fe600e8d1c9f9ec83fa
abienias97 <96074651+abienias97@users.noreply.github.com>
2024-12-08 13:53:35 +0100

    Feature Lab1 Zad4 241185 (#20)
    Co-authored-by: abienias97 <abienias97@gmail.com>

--- arithmetics.py
+++ arithmetics.py
@@ -5,7 +5,9 @@ class Arithmetics(ABC):
     @abstractmethod
     def difference(self, a: float, b: float) -> float:
         pass
-    
     @abstractmethod
     def multiplication(self, a: float, b: float) -> float:
+        pass
+    @abstractmethod
+    def divide(self, a: float, b: float) -> float:
         pass
--- calc.py
+++ calc.py
@@ -4,6 +4,9 @@ from arithmetics import Arithmetics
 class Calc(Arithmetics):
     def difference(self, a: float, b: float) -> float:
         return a - b
-
     def multiplication(self, a: float, b: float) -> float:
         return a * b
+    def divide(self, a: float, b: float) -> float:
+        if b == 0:
+            return 0
+        return a/b
