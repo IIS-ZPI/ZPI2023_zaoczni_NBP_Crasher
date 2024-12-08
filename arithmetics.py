9322082165d113f8ee76c5dccb8c5fba2fd04830
Smithski <62666278+GringoXY@users.noreply.github.com>
2024-12-08 14:01:19 +0100

    Feature lab1 zad4 241214 (#23)
    Co-authored-by: PK <przemyslawkowalski01pk@gmail.com>

--- arithmetics.py
+++ arithmetics.py
@@ -9,9 +9,19 @@ class Arithmetics(ABC):
     @abstractmethod
     def difference(self, a: float, b: float) -> float:
         pass
+
     @abstractmethod
     def multiplication(self, a: float, b: float) -> float:
         pass
+    
+    @abstractmethod
+    def power(self, a: float, b: float) -> float:
+        pass
+    
+    @abstractmethod
+    def power(self, a: float, b: int) -> float:
+        pass
+      
     @abstractmethod
     def divide(self, a: float, b: float) -> float:
         pass
--- calc.py
+++ calc.py
@@ -7,9 +7,14 @@ class Calc(Arithmetics):
 
     def difference(self, a: float, b: float) -> float:
         return a - b
+    
     def multiplication(self, a: float, b: float) -> float:
         return a * b
+    
     def divide(self, a: float, b: float) -> float:
         if b == 0:
             return 0
         return a/b
+    
+    def power(self, a: float, b: float) -> float:
+        return a ** b
