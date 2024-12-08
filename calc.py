9189eab214d94512ecc89756a4b48bbad1f8c333
Stanislaw Horna <stanislawhorna@outlook.com>
2024-12-08 13:50:11 +0100

    add Multiplication (#19)

--- calc.py
+++ calc.py
@@ -4,3 +4,6 @@ from arithmetics import Arithmetics
 class Calc(Arithmetics):
     def difference(self, a: float, b: float) -> float:
         return a - b
+
+    def multiplication(self, a: float, b: float) -> float:
+        return a * b
