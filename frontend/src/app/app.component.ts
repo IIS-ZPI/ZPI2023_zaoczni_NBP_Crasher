9768326e323bdcbca2b1736850b62934b3f3e9d6
GringoXY <przemyslawkowalski01pk@gmail.com>
2025-01-04 20:03:46 +0100

    prepare app for implementation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { appTitle } from './app.config';
+import { DxDateBoxModule } from 'devextreme-angular/ui/date-box';
+import { DxTabsModule } from 'devextreme-angular/ui/tabs';
+import { DxSelectBoxModule } from 'devextreme-angular/ui/select-box';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
+import { DxChartModule } from 'devextreme-angular/ui/chart';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    DxDateBoxModule,
+    DxTabsModule,
+    DxSelectBoxModule,
+    DxDataGridModule,
+    DxChartModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = appTitle;
 }
