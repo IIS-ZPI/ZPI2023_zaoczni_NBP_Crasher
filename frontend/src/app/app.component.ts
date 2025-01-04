91c2efd7114935cfc830a8c9ad5b8444df56e3e0
GringoXY <przemyslawkowalski01pk@gmail.com>
2025-01-04 22:56:52 +0100

    add changes distribution chart

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { appTitle } from './app.config';
+import { appName as appName, defaultCurrencyFrom, defaultCurrencyTo, defaultTimeFrame, defaultTimeFrameIndex, minDateFrom, tempData, timeFrames } from './app.config';
 import { DxDateBoxModule } from 'devextreme-angular/ui/date-box';
 import { DxTabsModule } from 'devextreme-angular/ui/tabs';
 import { DxSelectBoxModule } from 'devextreme-angular/ui/select-box';
 import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
 import { DxChartModule } from 'devextreme-angular/ui/chart';
+import { availableCurrencies, Currency, Data, TimeFrame } from './app.model';
 
 @Component({
   selector: 'app-root',
@@ -20,5 +21,58 @@ import { DxChartModule } from 'devextreme-angular/ui/chart';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = appTitle;
+
+  readonly appName = appName;
+  readonly availableCurrencies = availableCurrencies;
+  readonly timeFrames = timeFrames;
+  readonly minDateFrom = minDateFrom;
+
+  get isCustomTimeFrame(): boolean { 
+    return timeFrames[this.selectedTimeFrameIndex].badge === 'CUSTOM';
+  }
+
+  currencyFrom: Currency = defaultCurrencyFrom;
+  currencyTo: Currency = defaultCurrencyTo;
+
+  dateFrom!: Date;
+  dateTo!: Date;
+
+  selectedTimeFrameIndex: number = defaultTimeFrameIndex;
+  selectedTimeFrame: TimeFrame = defaultTimeFrame;
+  data: Data = tempData;
+
+  async refreshData(): Promise<void> {
+    try {
+      console.error('TODO');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  onCurrencyFromSelectionChanged(): void {
+    this.refreshData();
+  }
+
+  onCurrencyToSelectionChanged(): void {
+    this.refreshData();
+  }
+
+  onDateFromValueChanged(): void {
+    this.refreshData();
+  }
+
+  onDateToValueChanged(): void {
+    this.refreshData();
+  }
+
+  onTimeFrameIndexSelectionChanged(index: number): void {
+    this.selectedTimeFrameIndex = index;
+    this.selectedTimeFrame = timeFrames[this.selectedTimeFrameIndex];
+    
+    this.dateFrom = this.selectedTimeFrame.dateFrom!;
+    this.dateTo = this.selectedTimeFrame.dateTo!;
+
+    this.refreshData();
+  }
+  
 }
