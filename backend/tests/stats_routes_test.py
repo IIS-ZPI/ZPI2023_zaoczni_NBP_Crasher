4723e550dcb90bcfbbb2e623c81de91bfe0dab3a
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-07 21:23:42 +0100

    Add constant var for api url

--- backend/tests/stats_routes_test.py
+++ backend/tests/stats_routes_test.py
@@ -7,6 +7,7 @@ from backend.fastAPI import app
 
 client = TestClient(app)
 
+API_STATS_URL = "/api/stats/"
 
 @patch("backend.routes.stats_routes.get_currency_rates")
 class TestStatsRoutes(unittest.TestCase):
@@ -20,41 +21,41 @@ class TestStatsRoutes(unittest.TestCase):
             ],
         }
         response = client.get(
-            "/api/stats/?first_currency=usd&date_from=2023-01-01&date_end=2023-01-07"
+            f"{API_STATS_URL}?first_currency=usd&date_from=2023-01-01&date_end=2023-01-07"
         )
         self.assertEqual(response.status_code, 200)
         self.assertIn("statistics", response.json())
 
     def test_get_stats_invalid_date_range(self, mock_get_currency_rates):
         response = client.get(
-            "/api/stats/?first_currency=usd&date_from=2023-01-07&date_end=2023-01-01"
+            f"{API_STATS_URL}?first_currency=usd&date_from=2023-01-07&date_end=2023-01-01"
         )
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {"detail": "invalid_data"})
 
     def test_get_stats_date_not_supported(self, mock_get_currency_rates):
         response = client.get(
-            "/api/stats/?first_currency=usd&date_from=2000-01-01&date_end=2000-01-07"
+            f"{API_STATS_URL}?first_currency=usd&date_from=2000-01-01&date_end=2000-01-07"
         )
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {"detail": "date_not_supported"})
 
     def test_get_stats_date_in_future(self, mock_get_currency_rates):
         future_date = (datetime.today() + timedelta(days=3)).strftime("%Y-%m-%d")
-        url = f"/api/stats/?first_currency=usd&date_from=2023-01-01&date_end={future_date}"
+        url = f"{API_STATS_URL}?first_currency=usd&date_from=2023-01-01&date_end={future_date}"
         response = client.get(url)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {"detail": "invalid_data"})
 
     def test_get_stats_invalid_date_format(self, mock_get_currency_rates):
         response = client.get(
-            "/api/stats/?first_currency=usd&date_from=not-a-date&date_end=2023-01-07"
+            f"{API_STATS_URL}?first_currency=usd&date_from=not-a-date&date_end=2023-01-07"
         )
         self.assertEqual(response.status_code, 400)
 
     def test_get_stats_non_3letter_currency(self, mock_get_currency_rates):
         response = client.get(
-            "/api/stats/?first_currency=usdx&date_from=2023-01-01&date_end=2023-01-07"
+            f"{API_STATS_URL}?first_currency=usdx&date_from=2023-01-01&date_end=2023-01-07"
         )
         self.assertEqual(response.status_code, 422)
 
@@ -78,7 +79,7 @@ class TestStatsRoutes(unittest.TestCase):
             },
         ]
         response = client.get(
-            "/api/stats/?first_currency=USD&second_currency=EUR&date_from=2023-01-01&date_end=2023-01-07"
+            f"{API_STATS_URL}?first_currency=USD&second_currency=EUR&date_from=2023-01-01&date_end=2023-01-07"
         )
         self.assertEqual(response.status_code, 200)
         self.assertIn("statistics", response.json())
