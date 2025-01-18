e0190b85d301c4253071810368a3386652d94184
GringoXY <przemyslawkowalski01pk@gmail.com>
2025-01-18 23:42:07 +0100

    add error code

--- frontend/src/app/services/stats/stats.errors.ts
+++ frontend/src/app/services/stats/stats.errors.ts
@@ -3,4 +3,5 @@ import { ApiError } from '../../models/error.model';
 export const statsApiErrors: ApiError = {
   'invalid_data': 'Invalid data',
   'date_not_supported': 'Date not supported',
+  'currencies_are_equal': 'Selected currencies are equal (the same)',
 };
